1eeb48491f8b069055d08fff34d0068deeb62e5b
Jeshan04 <jeshxn04@gmail.com>
2024-10-25 11:27:48 +0530

    final

---
+++ assets/js/allfiles.js
@@ -0,0 +1,38 @@
+// Load jQuery
+const script1 = document.createElement('script');
+script1.src = 'vendor/jquery/jquery.min.js';
+document.head.appendChild(script1);
+
+// Load Bootstrap
+const script2 = document.createElement('script');
+script2.src = 'vendor/bootstrap/js/bootstrap.bundle.min.js';
+document.head.appendChild(script2);
+
+// Load other scripts
+const script3 = document.createElement('script');
+script3.src = 'assets/js/owl-carousel.js';
+document.head.appendChild(script3);
+
+const script4 = document.createElement('script');
+script4.src = 'assets/js/animation.js';
+document.head.appendChild(script4);
+
+const script5 = document.createElement('script');
+script5.src = 'assets/js/imagesloaded.js';
+document.head.appendChild(script5);
+
+const script6 = document.createElement('script');
+script6.src = 'assets/js/popup.js';
+document.head.appendChild(script6);
+
+const script7 = document.createElement('script');
+script7.src = 'assets/js/custom.js';
+document.head.appendChild(script7);
+
+const script8 = document.createElement('script');
+script8.src = 'assets/js/scrollbar.js';
+document.head.appendChild(script8);
+
+const script9 = document.createElement('script');
+script9.src = 'assets/js/header_footer.js';
+document.head.appendChild(script9);
